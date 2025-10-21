216261fe0a306116f6d189cb562133d8bd7be4dd
monlai <longmtse184140@fpt.edu.vn>
2025-10-21 19:25:12 +0700

    update return ủl

--- internal/models/db_models/feedback.go
+++ internal/models/db_models/feedback.go
@@ -1,16 +1,13 @@
 package db_models
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 )
 
 type Feedback struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	UserID    uuid.UUID `gorm:"type:uuid;not null"` // Reference to the user providing feedback
-	Comment   string    `gorm:"type:text;not null"`
-	Rating    int       `gorm:"type:int;not null;check:rating >= 1 AND rating <= 5"` // Rating between 1 and 5
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	BaseModel
+	UserID  uuid.UUID `gorm:"type:uuid;not null"` // Reference to the user providing feedback
+	Comment string    `gorm:"type:text;not null"`
+	Rating  int       `gorm:"type:int;not null;check:rating >= 1 AND rating <= 5"` // Rating between 1 and 5
+
 }
